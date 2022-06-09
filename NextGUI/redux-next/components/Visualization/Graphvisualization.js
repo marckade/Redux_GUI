cc220e85f09a8b0f57c6a3f233f4af564f4b1917
Alex <dandiviney@isu.edu>
2022-06-08 22:20:14 -0600

    dynamic arcset is working, revamped Visualization

--- NextGUI/redux-next/components/Visualization/Graphvisualization.js
+++ NextGUI/redux-next/components/Visualization/Graphvisualization.js
@@ -4,59 +4,36 @@ import { useEffect, useState } from "react";
 const Graphviz = dynamic(() => import("graphviz-react"), { ssr: false });
 
 
-const graphTest = `graph GRAPHCOLORING {  
-    node [style="filled"];  
-    a--b; 
-    b--c; 
-    c--a; 
-    d--a; 
-    d--e; 
-    e--a; 
-    f--a; 
-    f--g; 
-    a--g; 
-    h--a; 
-    h--i; 
-    a--i; 
-    a[fillcolor = White] 
-    b[fillcolor = White] 
-    c[fillcolor = White] 
-    d[fillcolor = White] 
-    e[fillcolor = White] 
-    f[fillcolor = White] 
-    g[fillcolor = White] 
-    h[fillcolor = White] 
-    i[fillcolor = White] 
-     }`;
+
 
 function Page(props) {
   let viz = "";
-
-  const [visualization, setVisualization]= useState('')
+  //console.log(props.DOT);
+  const graphTest = props.DOT;
+  //const [visualization, setVisualization]= useState('')
 
   // const [data, setData] = useState("");
 
-  useEffect(() => {
-   // console.log(`${props.visualization}\n`)
-    // getData();
-    draw();
-   //console.log(visualization)
-  }, [props]);
+  // useEffect(() => {
+  //  // console.log(`${props.visualization}\n`)
+  //   // getData();
+  //   //draw();
+  //  //console.log(visualization)
+  // }, [props]);
 
   
-  //problemName
-  function draw() {
-    //console.log("Draw \n")
-    viz = graphTest
-    setVisualization(graphTest)
+  // //problemName
+  // function draw() {
+  //   //console.log("Draw \n")
+  //   viz = graphTest
+  //   setVisualization(graphTest)
 
-    return
-  }
+  //   return
+  // }
 
 
 
   return (
-  
     <Graphviz dot={graphTest} width={200} height={200} />
 
   );
