5acb0fabda275ba5939352c0815d3a16c7893278
igbodani <igbodani@isu.edu>
2022-08-05 12:31:33 -0600

    Changed no-selection messages

--- NextGUI/redux-next/Tools/ProblemParser.js
+++ NextGUI/redux-next/Tools/ProblemParser.js
@@ -1,7 +1,7 @@
-// import  noReductionsMessage  from "../components/widgets/SearchBars/SearchBarSelectReduceToV2";
+
 
 // PROBLEMS AND THEIR VISUALIZATIONS
-let noReductionsMessage = 'No reductions available. Click on the add button to add a new reduction method';
+let noReductionsMessage = 'No reductions available. Click on the add button to add a new reduce-to';
 
 
 
@@ -13,7 +13,7 @@ wikiName.set('CLIQUE', 'Clique');
 wikiName.set('SUBSETSUM', 'Subset sum');
 wikiName.set('DM3', '3-dimensional matching');
 wikiName.set('KNAPSACK', 'Knapsack');
-wikiName.set('TSP', 'Traveling  salesman problem');
+wikiName.set('TSP', 'Traveling Salesperson Problem');
 wikiName.set('INTPROGRAMMING01', '0–1 integer programming');
 wikiName.set('VERTEXCOVER','Vertex cover');
 wikiName.set('GRAPHCOLORING', 'Graph Coloring');
@@ -38,7 +38,7 @@ export class ProblemParser {
         if(wikiName.has(problemName)){
             return wikiName.get(problemName);
         }
-
+       
         if(problemName === noReductionsMessage){
             return noReductionsMessage;
         }
